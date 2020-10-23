5f7eb055fc6cf7e3caff036547f92389a380a9dc
orange-1-1 <73345220+orange-1-1@users.noreply.github.com>
2020-10-23 20:29:01 +0800

    Add files via upload

---
+++ L1 homework1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c=128;
+    int f=c*9/5+32;
+    printf("Äá¤ó¬O:%d\n",a);
+    printf("µØ¤ó¬O:%d\n",c);
+
+    system("pause");
+
+    return 0;
+
+}
---
+++ L1 homework3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int x=10;
+   int y=4;
+   float a=x/y;
+   printf("x+y=%d\n",x+y);
+   printf("x-y=%d\n",x-y);
+   printf("x*y=%d\n",x*y);
+   printf("x/y=%f\n",a);
+   printf("x%y=%d\n",x%y);
+
+
+
+}
